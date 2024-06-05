5d0482189856aa7678f702ffebc6056f5c41c4e9
AND3SIL4 <afsilva6236@misena.edu.co>
2024-06-05 11:13:23 -0500

    feat(app): new login component has been created

---
+++ src/components/Login.tsx
@@ -0,0 +1,70 @@
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { toast } from 'sonner';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+const DATA = {
+  email: 'andres.silva@globalteksecurity.com',
+  password: 'Alemania2024*',
+};
+
+const LoginComponent: React.FC<string> = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    if (data.email === DATA.email && data.password === DATA.password) {
+      window.location.href = '/CRM';
+      toast.success('Login exitoso', { position: 'bottom-center' });
+    } else {
+      toast.info('Datos incorrectos', { position: 'bottom-center' });
+    }
+  };
+
+  return (
+    <form id="login-form" className="w-full" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm text-gray-900 dark:text-white">
+          Correo electronico
+        </label>
+        <input
+          {...register('email')}
+          type="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@globalteksecurity.com "
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm text-gray-900 dark:text-white">
+          Contraseña
+        </label>
+        <input
+          {...register('password')}
+          type="password"
+          id="password"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+          minLength={6}
+        />
+      </div>
+      <div>
+        <button
+          id="button"
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full"
+        >
+          Iniciar Sesión
+        </button>
+        <span className="text-[11px] dark:text-white/50 mt-18">
+          Developed by Felipe Silva
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default LoginComponent;
